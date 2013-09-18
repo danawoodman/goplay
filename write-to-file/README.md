30bb911cda7655e8fa120cc6512520de23846d5c
Dana Woodman <dana@danawoodman.com>
2013-09-18 13:39:07 -0700

    add write to file example

---
+++ write-to-file/README.md
@@ -0,0 +1,12 @@
+# Write to File
+
+Opens up an input file and writes it to an output file. World changing.
+
+# Usage
+
+As with most other of the examples, you build the `.go` file and run it:
+
+```bash
+go build
+./write-to-file
+```
---
+++ write-to-file/write-to-file.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "io"
+  "os"
+)
+
+func main() {
+  // open input file
+  input_file, error := os.Open("input.txt")
+  if error != nil { panic(error) }
+
+  // close file on exit and check for its returned error
+  defer func() {
+    if error := input_file.Close(); error != nil {
+      panic(error)
+    }
+  }()
+
+  // open output file
+  output_file, error := os.Create("output.txt")
+  if error != nil { panic(error) }
+  // close output_file on exit and check for its returned error
+  defer func() {
+      if error := output_file.Close(); error != nil {
+          panic(error)
+      }
+  }()
+
+  // make a buffer to keep chunks that are read
+  buffer := make([]byte, 1024)
+  for {
+    // read a chunk
+    n, error := input_file.Read(buffer)
+    if error != nil && error != io.EOF { panic(error) }
+    if n == 0 { break }
+
+    // write a chunk
+    if _, error := output_file.Write(buffer[:n]); error != nil {
+      panic(error)
+    }
+  }
+}
